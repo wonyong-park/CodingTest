4c41056d69c96f0bec3262ac28ecc81ed6ccccbc
wonyong-park <wanyong0919@naver.com>
2021-07-28 23:51:38 +0900

    2021-07-28 boj 1212 품

--- src/chapter4/boj/Solved1212.java
+++ src/chapter4/boj/Solved1212.java
@@ -1,10 +1,11 @@
+package chapter4.boj;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Solved1212 {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String arr[] = {"000", "001", "010", "011", "100", "101", "110", "111"};
         StringBuffer sb = new StringBuffer();
@@ -17,6 +18,5 @@ public class Main {
         }
 
         System.out.println(sb);
-
     }
 }
