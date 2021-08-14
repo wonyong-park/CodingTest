6621de4aa3570fabf936bc595d0c5d34b1f04600
wonyong-park <wanyong0919@naver.com>
2021-08-15 00:17:06 +0900

    2021-08-15 백준 11726 2xn 타일링

---
+++ src/chapter8/boj/Solved11726.java
@@ -0,0 +1,23 @@
+package chapter8.boj;
+
+import java.util.Scanner;
+
+public class Solved11726 {
+    public static int[] d = new int[1001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        d[1] = 1;
+        d[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            d[i] = (d[i - 1] + d[i - 2])%10007;
+        }
+
+        System.out.println(d[n]);
+
+    }
+
+}
