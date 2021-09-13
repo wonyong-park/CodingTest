07c354eeaf36185a6db6f026e7ca673b27436eec
wonyong-park <wanyong0919@naver.com>
2021-09-13 16:26:14 +0900

    2021-09-13 chapter13 모험가길드

---
+++ src/problem/chpater11/모험가길드.java
@@ -0,0 +1,32 @@
+package problem.chpater11;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 모험가길드 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        //정렬
+        Arrays.sort(nums);
+
+        int result = 0;
+        int count = 0;
+        for (int num : nums) {
+            count++;
+            if (num >= count) {
+                result++;
+                count = 0;
+            }
+        }
+
+        System.out.println("result = " + result);
+    }
+}
